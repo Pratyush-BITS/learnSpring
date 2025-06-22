b79e8217ac8c6d7d8720b9f45f3d4075497a237b
Pratyush Sinha <pratyush.sinha01@sap.com>
2025-06-22 17:12:14 +0530

    read property from application.properties

--- src/main/java/com/spring/demo/mycoolapp/rest/FunRectController.java
+++ src/main/java/com/spring/demo/mycoolapp/rest/FunRectController.java
@@ -1,14 +1,18 @@
 package com.spring.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRectController {
 
+    @Value("${config.owner}")
+    private String owner;
+
     @GetMapping("/")
     public String sayHello() {
-        return "Hello Praty!";
+        return String.format("%s says Hello!", owner);
     }
 
 }
